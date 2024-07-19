46b95ea6dbec98417a26694c7ff5dc693a1e0c30
arunpragash-periyasamy <arunpragash46@gmail.com>
2024-07-19 09:46:50 +0530

    Created a basic auth with the in memory authentication

--- src/main/java/com/arunpragash/car_rental/config/SecurityConfig.java
+++ src/main/java/com/arunpragash/car_rental/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.arunpragash.car_rental.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,10 +23,25 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Encode the password
+        String encodedPassword = passwordEncoder().encode("password");
+
+        UserDetails userDetails = User
+                .withUsername("user")
+                .password(encodedPassword)
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
     @Bean
